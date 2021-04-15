bc91427f183f737b70c888458ce875733e7b4834
DulmiRamesha123 <dulmiramesha98@gmail.com>
2021-04-15 23:47:45 +0530

    User class read user data

--- GadgetBadget/src/model/User.java
+++ GadgetBadget/src/model/User.java
@@ -14,62 +14,92 @@ public class User {
 		 try
 	 {
 		 Class.forName("com.mysql.jdbc.Driver");
-		
-		 //Provide the correct details: DBServer/DBName, username, password
+		 
+		 /*making the connection the connection
+		  * with database -> userdetailsdb */
+		 
 		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/userdetailsdb", "root", "");
 	 }
-	 catch (Exception e)
-		 {e.printStackTrace();}
-		 return con;
+	//catch exception when connection fail	 
+	 catch (Exception ex)
+		 {
+			 ex.printStackTrace();
+		 }
+		 
+	 return con;
 	 }
+	
+	//read all the user details from details 
+	//include all categories -> researchers , buyers ..
 	
 	public String readUsers()
 	 {
 			 String output = "";
-			 try
-			 {
-			 Connection con = connect();
-			 if (con == null)
-					{return "Error while connecting to the database for reading."; }
-					 // Prepare the html table to be displayed
-					 output = "<table border='1'><tr><th>User ID</th><th>Item Name</th>" +
-								 "<th>First Name</th>" +
-								 "<th>Last Name</th>" +
-								 "<th>Update</th><th>Remove</th></tr>";
-		
-					String query = "select * from user";
-					Statement stmt = con.createStatement();
-					ResultSet rs = stmt.executeQuery(query);
-					// iterate through the rows in the result set
-					while (rs.next())
-					{
-						String uID = Integer.toString(rs.getInt("uID"));
-						String firstName= rs.getString("firstName");
-						String lastName = rs.getString("lastName");
-						String dob =  rs.getString("dob");
-						String gender = rs.getString("gender");
-						String email = rs.getString("email");
-						String address = rs.getString("address");
-						// Add into the html table
-						output += "<tr><td>" + firstName + "</td>";
-						output += "<td>" + lastName + "</td>";
-						output += "<td>" + dob + "</td>";
-						output += "<td>" + gender + "</td>";
-						output += "<td>" + email + "</td>";
-						output += "<td>" + address + "</td>";
-						// buttons
-						output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"+ 
-						"<td><form method='post' action='users.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
-						+ "<input name='uID' type='hidden' value='" + uID + "'>" + "</form></td></tr>";
-						}
-						con.close();
-						// Complete the html table
-						output += "</table>";
+			 
+			 try{
+				 
+				 Connection con = connect();
+				 
+				 if (con == null)
+					 //if connection failed printing a error message
+						{
+					 		return "Error while connecting to the database for reading."; 
+					 	}
+				 
+						 // output as a table
+				 
+						output = "<table border='2'><B>"+
+								     "<tr><th>First Name</th>"+
+					                 "<th>Last Name</th>" +
+									 "<th>Date of Birth</th>" +
+									 "<th>Gender</th>" +
+									 "<th>Email</th>" +
+									 "<th>Address</th>" +
+									 "<th>Update</th><th>Remove</th></tr></B>";
+			
+						//get the data from database
+						
+						String query = "select * from user";
+						
+						Statement stmt = con.createStatement();
+						ResultSet rs = stmt.executeQuery(query);
+						
+						
+						while (rs.next())
+						{
+							String uID = Integer.toString(rs.getInt("uID"));
+							String firstName= rs.getString("firstName");
+							String lastName = rs.getString("lastName");
+							String dob =  rs.getString("dob");
+							String gender = rs.getString("gender");
+							String email = rs.getString("email");
+							String address = rs.getString("address");
+							
+							//put the details retrieved from database to above created html table
+							
+							output += "<tr><td>" + firstName + "</td>";
+							output += "<td>" + lastName + "</td>";
+							output += "<td>" + dob + "</td>";
+							output += "<td>" + gender + "</td>";
+							output += "<td>" + email + "</td>";
+							output += "<td>" + address + "</td>";
+							
+							// buttons
+							output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"+ 
+							"<td><form method='post' action='users.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+							+ "<input name='uID' type='hidden' value='" + uID + "'>" + "</form></td></tr>";
+							}
+							con.close();
+							
+							// close the above created html table
+							
+							output += "</table>";
 		}
+	    //catc exception
 		catch (Exception e)
 		{
-		output = "Error while reading the items.";
-		System.err.println(e.getMessage());
+			output = "Error while reading the items.";
+			System.err.println(e.getMessage());
 		}
 		return output;
 	 }
