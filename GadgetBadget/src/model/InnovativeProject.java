0992088ce7134628a878b18a9aa7b468250ae56f
vaji123 <it19126470@my.sliit.lk>
2021-04-19 10:10:54 -0230

    Add innovative project update part

--- GadgetBadget/src/model/InnovativeProject.java
+++ GadgetBadget/src/model/InnovativeProject.java
@@ -24,7 +24,7 @@ public class InnovativeProject {
 			 return con;
 			 }
 
-			public String insertItem(String code, String name, String desc, String skills, String budget,String no_of_funds,String amount)
+			public String insertItem(String rid, String code, String name, String desc, String skills, String budget,String no_of_funds,String amount)
 			 {
 				String output = "";
 				try
@@ -34,20 +34,21 @@ public class InnovativeProject {
 					{return "Error while connecting to the database for inserting."; }
 					
 					// create a prepared statement
-					String query = " insert into innovetive_project(`proj_id`,`proj_code`,`proj_name`,`proj_desc`,`skills_required`,`estimate_fund`,`no_of_funds_made`,`amount_to_fund`)"
-					 + " values (?, ?, ?, ?, ?, ?, ?, ?)";
+					String query = " insert into innovetive_project(`rid`,`proj_id`,`proj_code`,`proj_name`,`proj_desc`,`skills_required`,`estimate_fund`,`no_of_funds_made`,`amount_to_fund`)"
+					 + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			 
 					PreparedStatement preparedStmt = con.prepareStatement(query);
 			 
 					// binding values
 					preparedStmt.setInt(1, 0);
-					preparedStmt.setString(2, code);
-					preparedStmt.setString(3, name);
-					preparedStmt.setString(4, desc);
-					preparedStmt.setString(5, skills);
-					preparedStmt.setDouble(6, Double.parseDouble(budget));
-					preparedStmt.setDouble(7, Integer.parseInt(no_of_funds));
-					preparedStmt.setDouble(8, Double.parseDouble(budget));
+					preparedStmt.setString(2, rid);
+					preparedStmt.setString(3, code);
+					preparedStmt.setString(4, name);
+					preparedStmt.setString(5, desc);
+					preparedStmt.setString(6, skills);
+					preparedStmt.setDouble(7, Double.parseDouble(budget));
+					preparedStmt.setDouble(8, Integer.parseInt(no_of_funds));
+					preparedStmt.setDouble(9, Double.parseDouble(budget));
 					
 					// execute the statement
 
@@ -122,6 +123,41 @@ public class InnovativeProject {
 			 catch (Exception e)
 			 {
 			 output = "Error while reading the items.";
+			 System.err.println(e.getMessage());
+			 }
+			 return output;
+			 }
+			
+			public String updateItem(String ID, String rid, String code, String name, String desc, String skills, String budget, String no_of_funds, String amount)
+
+			 {
+			 String output = "";
+			 try
+			 {
+			 Connection con = connect();
+			 if (con == null)
+			 {return "Error while connecting to the database for updating."; }
+			 // create a prepared statement
+			 String query = "UPDATE innovetive_project SET rid=?,proj_code=?,proj_name=?,proj_desc=?,skills_required=?,estimate_fund=?,no_of_funds_made=?,amount_to_fund=?WHERE proj_id=?";
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 // binding values
+			 preparedStmt.setString(1, rid);
+			 preparedStmt.setString(2, code);
+			 preparedStmt.setString(3, name);
+			 preparedStmt.setString(4, desc);
+			 preparedStmt.setString(5, skills);
+			 preparedStmt.setDouble(6, Double.parseDouble(budget));
+			 preparedStmt.setDouble(7, Integer.parseInt(no_of_funds));
+			 preparedStmt.setDouble(8, Double.parseDouble(amount));
+			 preparedStmt.setInt(9, Integer.parseInt(ID));
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Updated successfully";
+			 }
+			 catch (Exception e)
+			 {
+			 output = "Error while updating the item.";
 			 System.err.println(e.getMessage());
 			 }
 			 return output;
