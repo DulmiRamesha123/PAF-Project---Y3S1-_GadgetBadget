b0d9dea8812f5b3772bd4a59760fb8800b30991f
chathura <chathurahsilva01@gmail.com>
2021-04-20 19:20:00 +0530

    Add insertBuyer method to buyer class

--- GadgetBadget/src/model/Buyer.java
+++ GadgetBadget/src/model/Buyer.java
@@ -16,4 +16,44 @@ public class Buyer {
    {e.printStackTrace();} 
     return con; 
    } 
+	 public String insertBuyer(String code, String name, String email, Integer buyerContactNumber, String address) 
+	   { 
+	     String output = ""; 
+	     try
+	    { 
+	     Connection con = connect(); 
+	     if (con == null) 
+	     {
+	   	  return "Error while connecting to the database for inserting."; 
+	      }
+	     
+	     // create a prepared statement
+	     
+	     String query = " insert into Buyer (`Buyer ID`,`Buyer Code`,`Buyer Name`,`Buyer Email`,`Buyer Contact Number`,`Buyer Address`)"+ " values (?, ?, ?, ?, ?, ?)"; 
+	     PreparedStatement preparedStmt = con.prepareStatement(query); 
+	     
+	    // binding values
+	    preparedStmt.setInt(1, 0); 
+	    preparedStmt.setString(2, code); 
+	    preparedStmt.setString(3, name); 
+	    preparedStmt.setString(4, email); 
+	    preparedStmt.setInt(5, buyerContactNumber); 
+	    preparedStmt.setString(6,address);
+	    
+	    System.out.println(code);
+	    System.out.println(name);
+	    
+	   // execute the statement3
+	   preparedStmt.execute(); 
+	   con.close(); 
+	   output = "Inserted successfully"; 
+	   } 
+	   catch (Exception e) 
+	   { 
+	   output = "Error while inserting buyer.";
+	   e.printStackTrace();
+	   //System.err.println(e.getMessage()); 
+	   } 
+	   return output; 
+	} 
 }
