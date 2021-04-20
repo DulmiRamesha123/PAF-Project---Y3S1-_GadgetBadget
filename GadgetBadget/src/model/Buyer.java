f7ff66f5cbd9893c90307881249b16c798d73f4d
chathura <chathurahsilva01@gmail.com>
2021-04-20 19:40:42 +0530

    Add readBuyer method

--- GadgetBadget/src/model/Buyer.java
+++ GadgetBadget/src/model/Buyer.java
@@ -56,4 +56,52 @@ public class Buyer {
 	   } 
 	   return output; 
 	} 
+
+	   public String readBuyer() 
+	{ 
+	   String output = ""; 
+	try
+	{ 
+	   Connection con = connect(); 
+	   if (con == null) 
+	{
+	   	return "Error while connecting to the database for reading."; 
+	} 
+	//Prepare the html table to be displayed
+	  output = "<table border='1'><tr><th>Buyer Code</th><th>Buyer Name</th>" + "<th>Buyer Email</th>" + "<th>Buyer Conatact Number</th>" +"<th>Buyer Address</th>" +"<th>Update</th><th>Remove</th></tr>"; 
+
+	   String query = "select * from Buyer"; 
+	   Statement stmt = con.createStatement(); 
+	   ResultSet rs = stmt.executeQuery(query); 
+	  // iterate through the rows in the result set
+	   while (rs.next()) 
+	{ 
+	   String BuyerID = Integer.toString(rs.getInt("Buyer ID")); 
+	   String BuyerCode = rs.getString("Buyer Code"); 
+	   String BuyerName = rs.getString("Buyer Name");
+	   String BuyerEmail = rs.getString("Buyer Email");
+	   String BuyerContactNumber = rs.getString("Buyer Contact Number"); 
+	   String BuyerAddress = rs.getString("Buyer Address"); 
+
+	   // Add into the html table
+	    output += "<tr><td>" + BuyerCode + "</td>"; 
+	    output += "<td>" + BuyerName + "</td>"; 
+	    output += "<td>" + BuyerEmail + "</td>"; 
+	    output += "<td>" + BuyerContactNumber + "</td>"; 
+	    output += "<td>" + BuyerAddress +"</td>";
+	    
+	   // buttons
+	   output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"+ "<td><form method='post' action='items.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"+ "<input name='itemID' type='hidden' value='" + BuyerID + "'>" + "</form></td></tr>"; 
+	 } 
+	   con.close(); 
+	   // Complete the html table
+	   output += "</table>"; 
+	 } 
+	   catch (Exception e) 
+	 { 
+	   output = "Error while reading Buyer."; 
+	   System.err.println(e.getMessage()); 
+	 } 
+	   return output; 
+	 } 
 }
