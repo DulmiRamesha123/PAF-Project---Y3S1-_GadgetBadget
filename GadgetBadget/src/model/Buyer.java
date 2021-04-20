c7d9c57a64b6bf71deec3e6b82c5d6018e68f492
chathura <chathurahsilva01@gmail.com>
2021-04-20 19:01:23 +0530

    Connect to Database

--- GadgetBadget/src/model/Buyer.java
+++ GadgetBadget/src/model/Buyer.java
@@ -1,5 +1,19 @@
 package model;
+import java.sql.*; 
 
 public class Buyer {
+	private Connection connect() 
+    { 
+      Connection con = null; 
+  try
+    { 
+    Class.forName("com.mysql.jdbc.Driver"); 
 
+     //Provide the correct details: DBServer/DBName, username, password 
+    con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/buyer", "root", ""); 
+    } 
+   catch (Exception e) 
+   {e.printStackTrace();} 
+    return con; 
+   } 
 }
