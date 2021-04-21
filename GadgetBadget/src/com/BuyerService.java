7426afeea6b3e33afa889d6cd57c761f8a980eb7
chathura <chathurahsilva01@gmail.com>
2021-04-21 11:01:13 +0530

    Create DELETE method in service class

--- GadgetBadget/src/com/BuyerService.java
+++ GadgetBadget/src/com/BuyerService.java
@@ -5,6 +5,7 @@ import model.Buyer;
 
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -12,6 +13,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -67,5 +71,19 @@ public class BuyerService
     
     String output = BuyerObj.updateBuyer(BuyerID,BuyerCode, BuyerName, BuyerEmail,BuyerContactNum,BuyerAddress);
     return output;
+   }
+   @DELETE
+   @Path("/") 
+   @Consumes(MediaType.APPLICATION_XML) 
+   @Produces(MediaType.TEXT_PLAIN) 
+   public String deleteBuyer(String BuyerData) 
+   { 
+   //Convert the input string to an XML document
+    org.jsoup.nodes.Document doc = Jsoup.parse( BuyerData, "", Parser.xmlParser()); 
+    
+   //Read the value from the element <itemID>
+    String BuyerID = doc.select("Buyer ID").text();
+    String output = BuyerObj.deleteBuyer(BuyerID);
+    return output; 
    }
 }
