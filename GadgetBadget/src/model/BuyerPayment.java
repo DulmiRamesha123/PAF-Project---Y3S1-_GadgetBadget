cd8b297e21e4886ac8f143c04c6880bb26cb7efc
ireshtharindu <ireshmeconi@gmail.com>
2021-04-22 17:30:23 +0530

    buyer connect to database

--- GadgetBadget/src/model/BuyerPayment.java
+++ GadgetBadget/src/model/BuyerPayment.java
@@ -9,6 +9,21 @@ import java.sql.Statement;
 
 public class BuyerPayment {
 	
+	//connect to database
+	
+			private Connection connect() {
+				Connection con = null;
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+
+					// Provide the correct details: DBServer/DBName, username, password
+					con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payment", "root", "");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return con;
+			}
+	
 	
 	
 
