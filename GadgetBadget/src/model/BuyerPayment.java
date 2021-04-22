c769a4bcdb21051ddedb78d322f654bec6d2dcae
ireshtharindu <ireshmeconi@gmail.com>
2021-04-22 17:31:55 +0530

    insert buyer payments

--- GadgetBadget/src/model/BuyerPayment.java
+++ GadgetBadget/src/model/BuyerPayment.java
@@ -24,6 +24,41 @@ public class BuyerPayment {
 				return con;
 			}
 	
+			//InsertPayments
+			
+			public String insertPayment(String cusid, String productid, String productname , String servicecg , String quantity , String totpay) {
+				String output = "";
+
+				try {
+					Connection con = connect();
+					if (con == null) {
+						return "Error while connecting to the database for inserting.";
+					}
+					// create a prepared statement
+					String query = " insert into bpaymenttb(`InvoiceID`,`CusID`,`ProductID`,`ProductName`,`ServiceCharge`,`Quantity`,`TotalPayment`)"
+							+ " values (?, ?, ?, ?, ?, ?, ?)";
+
+					PreparedStatement preparedStmt = con.prepareStatement(query);
+
+					// binding values
+					preparedStmt.setInt(1, 0);
+					preparedStmt.setString(2, cusid);
+					preparedStmt.setString(3, productid);
+					preparedStmt.setString(4, productname);
+					preparedStmt.setDouble(5, Double.parseDouble(servicecg));
+					preparedStmt.setDouble(6, Double.parseDouble(quantity));
+					preparedStmt.setDouble(7, Double.parseDouble(totpay));
+					// execute the statement
+
+					preparedStmt.execute();
+					con.close();
+					output = "Inserted successfully";
+				} catch (Exception e) {
+					output = "Error while inserting the payments.";
+					System.err.println(e.getMessage());
+				}
+				return output;
+			}
 	
 	
 
