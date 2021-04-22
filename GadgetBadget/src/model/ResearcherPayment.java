a12b2c9e371268c50e816f366bbe355234707fd1
ireshtharindu <ireshmeconi@gmail.com>
2021-04-22 16:57:01 +0530

    separate payment to buyer payment and reacher payment

---
+++ GadgetBadget/src/model/ResearcherPayment.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class ResearcherPayment {
+
+}
