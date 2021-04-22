1e3a002698820a012c067a3a10044fa8f74fdc7e
ireshtharindu <ireshmeconi@gmail.com>
2021-04-22 17:00:00 +0530

    reacher payment connect to database

--- GadgetBadget/src/model/ResearcherPayment.java
+++ GadgetBadget/src/model/ResearcherPayment.java
@@ -8,5 +8,21 @@ import java.sql.Statement;
 
 
 public class ResearcherPayment {
+	
+
+	//connect to database
+	
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payment", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 
 }
