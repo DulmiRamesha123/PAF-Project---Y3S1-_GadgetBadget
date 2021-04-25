6f32a118d0c046c46d1b514013fc5c3e19a1d569
Vajira Kavindi <68498264+vaji123@users.noreply.github.com>
2021-04-25 11:13:17 -0230

    Update Buyer.java
    Change the db name

--- GadgetBadget/src/model/Buyer.java
+++ GadgetBadget/src/model/Buyer.java
@@ -10,7 +10,7 @@ public class Buyer {
     Class.forName("com.mysql.jdbc.Driver"); 
 
      //Provide the correct details: DBServer/DBName, username, password 
-    con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/buyerdetailsdb", "root", ""); 
+    con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget", "root", ""); 
     } 
    catch (Exception e)  
    {e.printStackTrace();} 
