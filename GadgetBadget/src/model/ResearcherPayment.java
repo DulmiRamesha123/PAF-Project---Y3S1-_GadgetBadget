d4b143330d8dc39a5375eb45e7172a002cf6225a
Vajira Kavindi <68498264+vaji123@users.noreply.github.com>
2021-04-25 11:15:50 -0230

    Update ResearcherPayment.java
    Change the db name

--- GadgetBadget/src/model/ResearcherPayment.java
+++ GadgetBadget/src/model/ResearcherPayment.java
@@ -18,7 +18,7 @@ public class ResearcherPayment {
 			Class.forName("com.mysql.jdbc.Driver");
 
 			// Provide the correct details: DBServer/DBName, username, password
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payment", "root", "");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget", "root", "");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
