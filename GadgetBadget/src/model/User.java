3942d24721e03115d6868b4777c980bff8f1f22b
Vajira Kavindi <68498264+vaji123@users.noreply.github.com>
2021-04-25 11:16:18 -0230

    Update User.java
    Change the db name

--- GadgetBadget/src/model/User.java
+++ GadgetBadget/src/model/User.java
@@ -18,7 +18,7 @@ public class User {
 		 /*making the connection the connection
 		  * with database -> userdetailsdb */
 		 
-		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/userdetailsdb", "root", "");
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget", "root", "");
 	 }
 	//catch exception when connection fail	 
 	 catch (Exception ex)
